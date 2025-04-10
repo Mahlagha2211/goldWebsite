0949892553e14477722b76762e89bd5a23b6d25f
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-04-10 20:16:09 +0330

    update the part of lastarrival

--- src/components/Homepage/LastArrival.jsx
+++ src/components/Homepage/LastArrival.jsx
@@ -10,41 +10,51 @@ import { Navigation, Pagination } from "swiper/modules";
 export default function LastArrival() {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
-  const paginationRef = useRef(null);
+
   const products = [
     {
       id: 1,
       name: "necklace",
-      img: "./image/big photo card2(1).png",
+      img: "./image/arrival1.png",
+      explain:
+        "Now + Forever Lab-Grown Diamonds Princess-Cut Bridal Set 1-1/2 ct tw 14K Yellow Gold",
       price: 125.9,
     },
     {
       id: 2,
       name: "necklace",
-      img: "./image/big photo card.png",
+      img: "./image/arrival2.png",
+      explain:
+        "Now + Forever Lab-Grown Diamonds Princess-Cut Bridal Set 1-1/2 ct tw 14K Yellow Gold",
       price: 125.9,
     },
     {
       id: 3,
       name: "necklace",
-      img: "./image/big photo card2(2).png",
+      img: "./image/arrival3.png",
+      explain:
+        "Now + Forever Lab-Grown Diamonds Princess-Cut Bridal Set 1-1/2 ct tw 14K Yellow Gold",
       price: 125.9,
     },
     {
       id: 4,
       name: "necklace",
-      img: "./image/big photo card2(3).png",
+      img: "./image/arrival4.png",
+      explain:
+        "Now + Forever Lab-Grown Diamonds Princess-Cut Bridal Set 1-1/2 ct tw 14K Yellow Gold",
       price: 125.9,
     },
     {
       id: 5,
       name: "necklace",
-      img: "./image/big photo card2(4).png",
+      img: "./image/arrival5.png",
+      explain:
+        "Now + Forever Lab-Grown Diamonds Princess-Cut Bridal Set 1-1/2 ct tw 14K Yellow Gold",
       price: 125.9,
     },
   ];
   return (
-    <div className="bg-linear-to-b from-[#F2E9DB] via-[#E4D1B3] to-[#DFC8A5] flex flex-col items-center gap-y-10 py-16 h-[1000px]">
+    <div className="bg-linear-to-b from-[#F2E9DB] via-[#E4D1B3] to-[#DFC8A5] flex flex-col items-center gap-y-10 py-16 ">
       <div className="text-center space-y-4">
         <p className="text-4xl text-primaryFont">Last Arrivals</p>
         <p className="text-[#C0914B]">Indulge in what we offer</p>
@@ -52,13 +62,13 @@ export default function LastArrival() {
       <div className="w-2/3 relative ">
         <button
           ref={prevRef}
-          className="absolute z-10  -bottom-16 left-2  p-3 rounded-full shadow text-[#C0914B] border-2 border-[#C0914B] hover:bg-[rgba(255,255,255,0.5)]"
+          className="absolute z-10  bottom-1/2 -left-16  p-3 rounded-full shadow text-[#C0914B] border-2 border-[#C0914B] hover:bg-[rgba(255,255,255,0.5)]"
         >
           <GrFormPrevious />
         </button>
         <button
           ref={nextRef}
-          className="absolute z-10 -bottom-16 left-16  p-3 rounded-full shadow text-[#C0914B] border-2 border-[#C0914B] hover:bg-[rgba(255,255,255,0.5)]"
+          className="absolute z-10 bottom-1/2 -right-16  p-3 rounded-full shadow text-[#C0914B] border-2 border-[#C0914B] hover:bg-[rgba(255,255,255,0.5)]"
         >
           <GrFormNext />
         </button>
@@ -72,16 +82,6 @@ export default function LastArrival() {
           onBeforeInit={(swiper) => {
             swiper.params.navigation.prevEl = prevRef.current;
             swiper.params.navigation.nextEl = nextRef.current;
-            swiper.params.pagination.el = paginationRef.current;
-          }}
-          pagination={{ clickable: true, el: paginationRef.current }}
-          onSwiper={(swiper) => {
-            setTimeout(() => {
-              swiper.navigation.init();
-              swiper.navigation.update();
-              swiper.pagination.init();
-              swiper.pagination.update();
-            }, 0);
           }}
           breakpoints={{
             320: { slidesPerView: 1 },
@@ -89,30 +89,63 @@ export default function LastArrival() {
             1024: { slidesPerView: 3 },
           }}
         >
-          {products.map((product) => (
+          {products.map((product, index) => (
             <SwiperSlide key={product.id} className="space-y-3">
-              <div className="bg-[linear-gradient(to_top,rgba(255,255,255,0.6),rgba(255,255,255,0.1),rgba(255,255,255,0.6))]  p-2 rounded-2xl border border-white">
+              <div
+                className={`p-2 rounded-2xl border border-white ${
+                  index % 2 === 0
+                    ? "bg-[linear-gradient(to_top,rgba(255,255,255,0.6),rgba(255,255,255,0.1),rgba(255,255,255,0.6))] "
+                    : "bg-linear-to-t from-20% from-[rgba(194,137,120,0.5)]  via-70% via-[rgba(255,255,255,0.1)] to-100% to-[rgba(255,255,255,0.4)]"
+                } `}
+              >
                 <div className=" relative">
                   <img
                     src={product.img}
                     className="w-full h-full object-cover"
                   />
-                  <div className="absolute  backdrop-blur-2xl w-full text-center bottom-12">
+                  <div className="absolute w-full text-center bottom-0  p-4">
                     <p className="text-2xl">{product.name}</p>
+                    <p className="text-justify text-[13px]">{product.explain}</p>
                     <p className="">{product.price} $</p>
                   </div>
                 </div>
               </div>
-              <div className="w-full bg-[#C0914B] text-center rounded-2xl p-3 border text-white">
+              <div
+                className={`w-full  text-center rounded-2xl p-3 border text-white cursor-pointer ${
+                  index % 2 === 0
+                    ? "bg-[#a87830] hover:bg-[#C0914B]"
+                    : "bg-[rgba(194,137,120,0.8)] border-[#C28978] hover:bg-[rgb(197,133,114)]"
+                }`}
+              >
                 Add to CART
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
-        <div
-          ref={paginationRef}
-          className="absolute  space-x-4 -bottom-[52px] left-1/2"
-        ></div>
+      </div>
+      <div className="w-full my-32 grid grid-cols-2 relative">
+        <div className="bg-linear-to-t from-[rgba(255,255,255,0.6)] via-[rgba(255,255,255,0.1)] to-[rgba(255,255,255,0.6)]  border border-white p-20 space-y-4 h-80 flex justify-center  flex-col">
+          <p className="text-4xl text-[#806132]">
+            <span className="text-6xl">Gifts</span> Of The Season
+          </p>
+          <p className="text-[#604825]">
+            When it comes to celebrations we strive to make your experience as
+            brilliant as our jewelry with the perfect pieces for every occasion.
+          </p>
+          <a
+            href=""
+            className="bg-[#C0914B] text-white p-2 rounded-lg text-sm border self-baseline"
+          >
+            EXPLORE NOW
+          </a>
+        </div>
+        <div className="bg-linear-to-t from-[rgba(255,255,255,0.6)] via-[rgba(255,255,255,0.1)] to-[rgba(255,255,255,0.6)] rounded-tl-2xl rounded-bl-2xl py-2 pl-2  absolute w-[700px] h-96 right-0 -top-7 border border-white ">
+          <img
+            src="./image/necklace3.png"
+            alt=""
+            className="rounded-tl-2xl rounded-bl-2xl w-full h-full  "
+          />
+        </div>
       </div>
     </div>
   );
