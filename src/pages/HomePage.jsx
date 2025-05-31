1313d532e73b0310c99139560e2044a145fccdb9
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-05-31 20:20:45 +0330

    edit the path

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Homepage/Header";
 import Category from "../components/Homepage/Category";
 import LastArrival from "../components/Homepage/LastArrival";
 import Diamond from "../components/Homepage/Diamond";
-import Opinion from "../components/Homepage/opinion";
+import Opinion from "../components/Homepage/Opinion";
 import Footer from "../components/Homepage/Footer";
 
 export default function HomePage() {
